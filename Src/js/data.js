3ab8519847bca1628361e4d6be91f9d5f54cc0c9
Alex-Beltran97 <92172762+Alex-Beltran97@users.noreply.github.com>
2022-01-11 13:55:01 -0500

    Se actualiza los directorios de imagenes en la data

--- Src/js/data.js
+++ Src/js/data.js
@@ -8,61 +8,61 @@ export const pages = [
     {
         name:"Presentation-card.",
         Description:"Aquí podras ver el diseño de una targeta de opresetancion demasiado basica.",
-        image:"../Style/images/Presetation card.JPG",
+        image:"https://scontent.fbog4-2.fna.fbcdn.net/v/t39.30808-6/271669681_1361588600926741_4722794582977594385_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=-_o5YMUdIVYAX-oXTld&_nc_ht=scontent.fbog4-2.fna&oh=00_AT9pe3swfufKbOa5GAA51ml2Jv7aFby9Ku_N90BtlChqPw&oe=61E34698",
         url: "https://alex-beltran97.github.io/Presentation-card/"
     },
     {
         name:"Consumiendo una API: Generador de perritos.",
         Description:"Aquí podras generar diferentes imagenes de perritos con tan solo pulsar un botón.",
-        image:"../Style/images/Generador de perritos.JPG",
+        image:"https://scontent.fbog4-1.fna.fbcdn.net/v/t39.30808-6/270260719_1361588484260086_8499165751111057629_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=tEs465sb09kAX_i2Qfo&tn=q1-T--HTEseKj-eX&_nc_ht=scontent.fbog4-1.fna&oh=00_AT-YZ9uWA_upLVLdgkfqZot7mGt9IuBopUqAXBscc1eoxw&oe=61E36434",
         url: "https://alex-beltran97.github.io/Generador-de-perritos/"
     },
     {
         name:"Formulario de registro funcional.",
         Description:"Aquí podras ingresar tu nombre, apellido y edad para que estos se vean reflejados en pantalla.",
-        image:"../Style/images/Formulario de registro -1.JPG",
+        image:"https://scontent.fbog4-1.fna.fbcdn.net/v/t39.30808-6/271288522_1361588450926756_1572972759017263858_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=Uu2GeA9Ki0wAX8K4SKI&_nc_ht=scontent.fbog4-1.fna&oh=00_AT-Bq9YVCa_Lzn3KIKCEnjIkkTuRwU2gQTb0V_ZBHv6hNQ&oe=61E33CEE",
         url: "https://alex-beltran97.github.io/Generador-de-perritos/"
     },
     {
         name:"Page: Error 404 (Not-Found).",
         Description:"Aquí podras ver una maqueta de como luciria una pagina de Error 404.",
-        image:"../Style/images/Error 404 not found.JPG",
+        image:"https://scontent.fbog4-1.fna.fbcdn.net/v/t39.30808-6/270054656_1361588380926763_6464681139073690862_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=3iFKF40Yll0AX_5UB7e&tn=q1-T--HTEseKj-eX&_nc_ht=scontent.fbog4-1.fna&oh=00_AT89f0A20cWqDpsiBhpc0zyecIjftkJbnhhIz_4O0MjJRA&oe=61E37913",
         url: "https://alex-beltran97.github.io/Alex-Beltran97-Page-Error-404-Not-Found/"
     },
     {
         name:"Landing page, freeCodeCamp.",
         Description:"Aquí podras ver una maqueta de la landing page de freeCodeCamp.",
-        image:"../Style/images/FreeCodeCamp.JPG",
+        image:"https://scontent.fbog4-1.fna.fbcdn.net/v/t39.30808-6/270233633_1361588467593421_4456074452586749579_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=7EoSoX-QkyIAX-cZEC6&_nc_ht=scontent.fbog4-1.fna&oh=00_AT-katFuuu9gMKw6vfU7I582xr34eHa50YidRpz0RikTkQ&oe=61E33508",
         url: "https://alex-beltran97.github.io/Imitacion-Inicio-freeCodeCamp/"
     },
     {
         name:"Calculadora para principiante.",
         Description:"Aquí encontraras el diseño de ua calculadora totalmente funcional.",
-        image:"../Style/images/Calculadora basica.JPG",
+        image:"https://scontent.fbog4-1.fna.fbcdn.net/v/t39.30808-6/269940640_1361588407593427_5999031820385004573_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=C4wVjuu7RvAAX9Mldsq&_nc_ht=scontent.fbog4-1.fna&oh=00_AT9Ej5Ix93y1QiIVPLDLbWptpZ6z186W9Peo37-JMwNtbQ&oe=61E29F7B",
         url: "https://alex-beltran97.github.io/calculadora/"
     },
     {
         name:"Landing page básica.",
         Description:"Aquí podras observar el diseño básico de una landing page con su repectiva semantica.",
-        image:"../Style/images/Landing page basica.JPG",
+        image:"https://scontent.fbog4-2.fna.fbcdn.net/v/t39.30808-6/271645974_1361588537593414_5782349131619806758_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=730e14&_nc_ohc=hAAp9riU8_wAX-KjeDH&tn=q1-T--HTEseKj-eX&_nc_ht=scontent.fbog4-2.fna&oh=00_AT8yYjpJzCniOStKEVmGrqJD2VcHhwC3J1wnXyG6G6x6Uw&oe=61E1C97A",
         url: "https://alex-beltran97.github.io/pagina_prueba/"
     },
     {
         name:"Maqueta de formulartio de registro.",
         Description:"Aquí podras observar la maqueta de un formulario de registro básico.",
-        image:"../Style/images/Maqueta de formulario.png",
+        image:"https://scontent.fbog4-1.fna.fbcdn.net/v/t39.30808-6/270039493_1361588527593415_2892560053833467389_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=MaLeXe_6mawAX9M3Uqi&_nc_ht=scontent.fbog4-1.fna&oh=00_AT-wLWoNAQrmimFLgH5oK1WvgWRf3LGvcCtIqL52HrcbZQ&oe=61E1CAD9",
         url: "https://alex-beltran97.github.io/Formulario-Registro/"
     },
     {
         name:"Reto técnico Sofka: ¿Quíen quiere ser millonario?.",
         Description:"Aquí podras jugar a quien quiere ser millonario.Diseñado para Sofka.",
-        image:"../Style/images/Sofka.png",
+        image:"https://scontent.fbog4-2.fna.fbcdn.net/v/t39.30808-6/271647404_1361588610926740_7666161173216803562_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=S_U0ID4bcQoAX_GdeY7&_nc_ht=scontent.fbog4-2.fna&oh=00_AT-drfZEf4p70h2sqHXiEediOk_toy0NAVHJGJR9OtiWig&oe=61E2E898",
         url: "https://alex-beltran97.github.io/Reto-tecnico-sofka-2.0/"
     },
     {
         name:"Página web para parejas.",
         Description:"Aquí podras regalarle un pequeño detalle a tu pareja con una serie de divertidas preguntas y mensajes romanticos.",
-        image:"../Style/images/Pagina para parejas.JPG",
+        image:"https://scontent.fbog4-2.fna.fbcdn.net/v/t39.30808-6/271027867_1361588547593413_4904776658828601409_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_ohc=TfYI7ZNjJ74AX-E6Mn1&_nc_ht=scontent.fbog4-2.fna&oh=00_AT8Kz95HqaAkkokdHHgp7O3r6cCKy7BhiMQwJNdeMBqhUQ&oe=61E3777D",
         url: "https://alex-beltran97.github.io/pagina-hecha-para-parejas/"
     },
 
